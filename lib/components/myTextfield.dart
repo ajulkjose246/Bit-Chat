9fe6d6e130a3f83203edfba88bc650a1bc493009
ajulkjose246 <mail.ajulkjose@gmail.com>
2024-03-09 09:21:54 +0530

    chat msg auto scroll added and chat app is working perfect

--- lib/components/myTextfield.dart
+++ lib/components/myTextfield.dart
@@ -4,17 +4,21 @@ class myTextfield extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
   final bool obsecuredText;
+  final FocusNode? focusNode;
+
   const myTextfield(
       {super.key,
       required this.controller,
       required this.hintText,
-      required this.obsecuredText});
+      required this.obsecuredText,
+      this.focusNode});
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
       obscureText: obsecuredText,
+      focusNode: focusNode,
       decoration: InputDecoration(
           hintText: hintText,
           enabledBorder: OutlineInputBorder(
--- lib/home/pages/chatPage.dart
+++ lib/home/pages/chatPage.dart
@@ -5,28 +5,71 @@ import 'package:bitchat/services/chat_services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   final String receiverEmail;
   final String receiverId;
   ChatPage({super.key, required this.receiverEmail, required this.receiverId});
 
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
   //text controller
   final TextEditingController _messageController = TextEditingController();
 
   //chat and auth services
-
   final ChatService _chatService = ChatService();
   final AuthService _authService = AuthService();
 
-  //send message
+  //textfield focus
 
+  FocusNode myFocusNode = FocusNode();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //add a listener to focus
+    myFocusNode.addListener(() {
+      if (myFocusNode.hasFocus) {
+        Future.delayed(
+          Duration(milliseconds: 500),
+          () => scrollDown(),
+        );
+      }
+    });
+
+    Future.delayed(Duration(milliseconds: 500), () => scrollDown());
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    myFocusNode.dispose();
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  //scroll controller
+  final ScrollController _scrollController = ScrollController();
+  void scrollDown() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  //send message
   void sendMessage() async {
     if (_messageController.text.isNotEmpty) {
-      await _chatService.sendMessage(receiverId, _messageController.text);
+      await _chatService.sendMessage(
+          widget.receiverId, _messageController.text);
       //clear message
 
       _messageController.clear();
     }
+    scrollDown();
   }
 
   @override
@@ -34,7 +77,7 @@ class ChatPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color.fromRGBO(233, 229, 225, 1),
       appBar: AppBar(
-        title: Text(receiverEmail),
+        title: Text(widget.receiverEmail),
       ),
       body: Column(
         children: [
@@ -52,7 +95,7 @@ class ChatPage extends StatelessWidget {
   Widget _buildMessageList() {
     String senderId = _authService.getCurrentUser()!.uid;
     return StreamBuilder(
-      stream: _chatService.getMessages(receiverId, senderId),
+      stream: _chatService.getMessages(widget.receiverId, senderId),
       builder: (context, snapshot) {
         //error
         if (snapshot.hasError) {
@@ -71,14 +114,15 @@ class ChatPage extends StatelessWidget {
         //return list
 
         return ListView(
+          controller: _scrollController,
           children:
               snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
         );
       },
     );
   }
-  //build Message Item
 
+  //build Message Item
   Widget _buildMessageItem(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
 
@@ -100,21 +144,23 @@ class ChatPage extends StatelessWidget {
   //builder msg input
   Widget _buildUserInput() {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 15),
+      padding: const EdgeInsets.only(bottom: 15, left: 10, right: 10),
       child: Row(
         children: [
           Expanded(
             child: myTextfield(
-                controller: _messageController,
-                hintText: "Type a message",
-                obsecuredText: false),
+              controller: _messageController,
+              hintText: "Type a message",
+              obsecuredText: false,
+              focusNode: myFocusNode,
+            ),
           ),
           Container(
             decoration: const BoxDecoration(
               color: Colors.green,
               shape: BoxShape.circle,
             ),
-            margin: const EdgeInsets.only(right: 25, left: 25),
+            margin: const EdgeInsets.only(left: 25),
             child: IconButton(
                 onPressed: sendMessage,
                 color: Colors.white,
